d955288031ad485b4072059c7c3f045238d4ecc9
DonkeyCo <duc.vongoc01@gmail.com>
2019-05-31 19:09:41 +0200

    Change to Node js completed, API access for github repos and langs

--- index.js
+++ index.js
@@ -1,10 +1,109 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var port = 8080;
+const express = require('express');
+const request = require("request");
+const app = express();
+const server = require('http').createServer(app);
+const port = 8080;
+const config = require("./config.json");
 
-server.listen(port, function() {
-    console.log("Webserver läuft und hört auf Port %d", port)
+//Start listening to port 8080
+app.listen(8080, function() {
+    console.log("Webserver läuft...")
 })
 
+//Root directory
 app.use(express.static(__dirname + "/public"))
+
+//Create a header to authenticate to Github API
+const header = {
+    "User-Agent": "DonkeyCo",
+    "Authorization": "token " + config["production"]["github"]
+}
+
+//Request for all public repositories
+const optionsList = {
+    url: "https://api.github.com/user/repos?type=public",
+    method: "GET",
+    headers: header
+}
+
+var responses = []
+request(optionsList, function(err, res, body) {
+    //Request for my public repositories
+    var result = JSON.parse(body);
+    //show the result at [url]/api/github-repos
+    app.get("/api/github-repos", function(req, res) {
+        res.send(result)
+    })
+
+    //Determine languages of repositories
+    //get a list of allrepositories
+    var repoList = getRepoList(result);
+    //list of all responses [needed because of async] (preparation) + counter
+    var responses = {repo_count: repoList.length, repos: {}};
+    var c_requests = 0;
+    //Iterate through all Repositories and send a request
+    for(var i = 0; i < repoList.length; i++) {
+        //pattern for all requests regarding the used languages
+        var langList = {
+            url: "https://api.github.com/repos/DonkeyCo/" + repoList[i] +"/languages",
+            method: "GET",
+            headers: header
+        }
+        //Request for the used languages in one repository
+        request(langList, function(err, res, body) {
+            //retrieve the name of the repository
+            var name = res.request.path.replace("/repos/DonkeyCo/", "").replace("/languages", "");
+            var result = JSON.parse(body);
+
+            //save the keys inside of a variable
+            var resultKeys = Object.keys(result);
+            var responsesKeys = Object.keys(responses);
+            //if there are no keys, it means the repository has no language -> category: other
+            if(resultKeys.length == 0) {
+                //if an item already exists in this category, push to its list, else create a new list
+                if(keyExists("Other", responses)) {
+                    responses["repos"]["Other"].push(name);
+                } else {
+                    responses["repos"]["Other"] = [name];
+                }
+            } else {
+                //same procedure, but iterating through all keys
+                for(var rKey in result) {
+                    if(keyExists(rKey, responses)) {
+                        responses["repos"][rKey].push(name);
+                    } else {
+                        responses["repos"][rKey] = [name];
+                    }
+                }
+            }
+            //increment counter
+            c_requests++;
+            //if counter equals count of all repositories, all requests are finished
+            if(c_requests == repoList.length) {
+                //create a page accessible with [url]/api/github-repos.lang where the result can be seen
+                app.get("/api/github-repos-lang", function(req, res) {
+                    res.send(responses);
+                })
+            }
+        })
+    }
+})
+
+/** Returns a list of all repository names */
+function getRepoList(result) {
+    var repoList = [];
+    for(var i = 0; i < result.length; i++) {
+        repoList.push(result[i]["name"])
+    }
+    return repoList;
+}
+
+/** Checks if the key already exists inside of the given list */
+function keyExists(key, responses) {
+    for(var respKey in responses["repos"]) {
+        if(respKey.localeCompare(key) == 0) { 
+            return true;
+        }
+    }
+    return false;
+}
--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
     $("#content").children().each(function() {
         contentItems.push($(this).attr("id"));
     })
+    var repos = getRepos();
+    var repoLangs = getRepoLanguages(repos);
 })
 
 $(window).scroll(function() {
@@ -22,3 +24,43 @@ $(window).scroll(function() {
         $("#title_wrapper").addClass("stay");
     }
 })
+
+function getRepos() {
+    var list = [];
+    $.ajax({
+        type: "GET",
+        url: "/api/github-repos",
+        async: false,
+        contentType: 'application/json',
+        success: function(data) {
+            for(var i = 0; i < data.length; i++) {
+                list.push(data[i]["name"]);
+            }
+        }
+    })
+    return list;
+}
+
+function getRepoLanguages(repos) {
+    var list = [];
+    for(var i = 0; i < repos.length; i++) {
+        $.ajax({
+            type: "GET",
+            url: "/api/github-repo-lang",
+            contentType: 'application/json',
+            data: {
+                name: repos[i]
+            },
+            success: function(data) {
+                var keys = Object.keys(data);
+                for(var j = 0; j < keys.length; j++) {
+                    if(list.indexOf(keys[j]) > -1) {
+                        list[keys[j]].push(repos[i]);
+                    } else {
+                        list[keys[j]] = [repos[i]]
+                    }
+                }
+            }
+        })
+    }
+}
